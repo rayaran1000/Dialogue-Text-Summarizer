458b8a6b40e125ab0b5d7991473ef4311c2164a3
Aranya Ray <aranya.ray1998@gmail.com>
2024-01-29 18:23:49 +0530

    Prediction pipeline and Application.py added

---
+++ src/textSummarizer/pipeline/prediction.py
@@ -0,0 +1,22 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluator_config() # Used to extract model path and tokenizer configuration
+
+    def predict(self,text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128} # First two parameters are default , max_length signifies the max length of the summary
+
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+
+        print("Dialogue:")
+        print(text)
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        print("\nModel Summary:")
+        print(output)
+
+        return output
